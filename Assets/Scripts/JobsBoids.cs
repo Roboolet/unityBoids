03109a1dfd2cf8d231dbb98413b3a67720370915
Lea <39883637+Roboolet@users.noreply.github.com>
2024-11-21 16:19:31 +0100

    Did some more jobbing, currently throws errors

--- Assets/Scripts/JobsBoids.cs
+++ Assets/Scripts/JobsBoids.cs
@@ -27,12 +27,21 @@ public class JobsBoids : MonoBehaviour
     // data representing each boid
     // order of data must NOT be modified, as each index corresponds to a single boid
     private NativeArray<float3> boidPositions; 
+    private NativeArray<float3> newBoidPositions;
     private NativeArray<float3> boidVelocities;
-    private NativeArray<int> boidInstanceIDs;
+    private NativeArray<float3> newBoidVelocities;
+    private int[] boidInstanceIDs;
     private Dictionary<int, GameObject> instanceMappings = new Dictionary<int, GameObject>();
     
     void Start()
     {
+        // set up arrays
+        boidPositions = new NativeArray<float3>(initialSpawnAmount, Allocator.Persistent);
+        newBoidPositions = new NativeArray<float3>(initialSpawnAmount, Allocator.Persistent);
+        boidVelocities = new NativeArray<float3>(initialSpawnAmount, Allocator.Persistent);
+        newBoidVelocities = new NativeArray<float3>(initialSpawnAmount, Allocator.Persistent);
+        boidInstanceIDs = new int[initialSpawnAmount];
+        
         // populate and set up the boids
         for (int i = 0; i < initialSpawnAmount; i++)
         {
@@ -41,11 +50,29 @@ public class JobsBoids : MonoBehaviour
             
             GameObject instance = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
             instance.transform.localScale = new Vector3(0.8f, 0.5f, 0.8f);
+            instanceMappings.Add(instance.GetInstanceID(), instance);
+            boidInstanceIDs[i] = instance.GetInstanceID();
         }
     }
 
     void FixedUpdate()
     {
+        // make and schedule job
+        JobsBoidsJob job = new JobsBoidsJob()
+        {
+            oldPositions = boidPositions,
+            oldVelocities = boidVelocities,
+            
+            newPositions = newBoidPositions,
+            newVelocities = newBoidVelocities,
+        };
+        JobHandle jobHandle = job.Schedule(initialSpawnAmount, 64);
+        jobHandle.Complete();
+        
+        // update the boid positions
+        boidPositions = job.newPositions;
+        boidVelocities = job.newVelocities;
+        
         RefreshInstanceObjects();
     }
 
@@ -61,10 +88,22 @@ public class JobsBoids : MonoBehaviour
                 Quaternion.LookRotation(Vector3.forward,boidVelocities[i]);
         }
     }
+
+    private void OnDestroy()
+    {
+        boidPositions.Dispose();
+        boidVelocities.Dispose();
+    }
 }
 
 public struct JobsBoidsJob : IJobParallelFor
 {
+    [ReadOnly] public NativeArray<float3> oldVelocities;
+    [ReadOnly] public NativeArray<float3> oldPositions;
+
+    [WriteOnly] public NativeArray<float3> newVelocities;
+    [WriteOnly] public NativeArray<float3> newPositions;
+    
     public void Execute(int index)
     {
         
